95d76a67282986c10fc3127bc2c49981bd2768c0
Fernando Chicaiza <washopilot@gmail.com>
2026-05-27 01:50:50 -0500

    juan_pardo_zumbi_26_05_2026

--- helpers.py
+++ helpers.py
@@ -73,7 +73,7 @@ def clean_list_tuples(lst: list, cols: tuple):
     result = []
 
     for _tpl in lst:
-        _conditions_to_exclude = ['Herramienta Menor (5% de Mano de Obra)', 'herramientas menores', 'Herramienta menor', 'Herramientas eléctricas', 'Herramienta liviana', 'Equipo liviano', 'Herramienta manual y menor de construcción', 'Herramienta Menor 5%MO', 'Herramienta manual y menor de construcción', 'Herramienta manual y menor de metalmecanica', 'Herramienta manual y menor de plomería', 'Herramienta Menor (5% M.O.)', 'Herramienta Menor (5% M.O.)' ]
+        _conditions_to_exclude = ['Herramienta Menor (5% de Mano de Obra)', 'herramientas menores', 'Herramienta menor', 'Herramientas eléctricas', 'Herramienta liviana', 'Equipo liviano', 'Herramienta manual y menor de construcción', 'Herramienta Menor 5%MO', 'Herramienta manual y menor de construcción', 'Herramienta manual y menor de metalmecanica', 'Herramienta manual y menor de plomería', 'Herramienta Menor (5% M.O.)', 'Herramienta Menor (5% M.O.)', 'HERRAMIENTA MENOR 5% M.O.' ]
         if any(condition in _tpl for condition in _conditions_to_exclude):
             _resulting_tuple = (_tpl[cols[0]],)
         else:
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from helpers import clean_list_tuples, get_tuples_between_tags, listing_sheet, t
 tcpu0 = time.time()
 
 # wb = load_workbook(filename='original.xlsx', read_only=True, data_only=True)
-wb = load_workbook(filename='original_large.xlsx', read_only=True, data_only=True)
+wb = load_workbook(filename='original.xlsx', read_only=True, data_only=True)
 
 # Inicialización de data
 listing_data = []  # Aquí se almacena todos los apus
@@ -19,18 +19,18 @@ for sheet in wb.sheetnames:
     if sheet == 'Rubros':
         continue
 
-    _detailedSheet = listing_sheet(wb[sheet], 1, 6)
+    _detailedSheet = listing_sheet(wb[sheet], 1, 10)
     
     print(sheet)
 
     equipment_list = equipment_list + clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'EQUIPOS', 'MANO DE OBRA', True, True), (1, 3))
+        _detailedSheet, 'EQUIPOS', 'MANO DE OBRA', True, True), (0, 3))
     labour_list = labour_list + clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'MANO DE OBRA', 'MATERIALES', True, True), (1, 3))
+        _detailedSheet, 'MANO DE OBRA', 'MATERIALES', True, True), (0, 4))
     materials_list = materials_list + clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'MATERIALES', 'TRANSPORTE', True, True), (1, 2, 4))
+        _detailedSheet, 'MATERIALES', 'TRANSPORTE', True, True), (0, 1, 4))
     transport_list = transport_list + clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'TRANSPORTE', '', True, True), (1, 2, 4))
+        _detailedSheet, 'TRANSPORTE', '', True, True), (0, 1, 3))
 
     # print(tabulate(equipment_list))
 
@@ -44,43 +44,43 @@ clean_materials_dict = {index: value for index,
 clean_transport_dict = {index: value for index,
                         value in enumerate(sorted(set(transport_list)), 3278)}
 
-# print(tabulate(clean_equipment_dict.items()))
-# print(tabulate(clean_labour_dict.items()))
-# print(tabulate(clean_materials_dict.items()))
-# print(tabulate(clean_transport_dict.items()))
+print(tabulate(clean_equipment_dict.items()))
+print(tabulate(clean_labour_dict.items()))
+print(tabulate(clean_materials_dict.items()))
+print(tabulate(clean_transport_dict.items()))
 
 # Segunda vuelta de rubros
 for sheet in wb.sheetnames:
-    if sheet == 'Rubros':
-        continue
+    # if sheet == 'Rubros':
+    #     continue
 
-    _detailedSheet = listing_sheet(wb[sheet], 1, 6)
+    _detailedSheet = listing_sheet(wb[sheet], 1, 10)
     # print(_detailedSheet)
 
     _equipment_list = clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'EQUIPOS', 'MANO DE OBRA', True, True), (1, 2, 3))
+        _detailedSheet, 'EQUIPOS', 'MANO DE OBRA', True, True), (0, 1, 3))
     _new_equipment_list = transform_tuples(
         clean_equipment_dict, _equipment_list)
 
     _labour_list = clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'MANO DE OBRA', 'MATERIALES', True, True), (1, 2, 3))
+        _detailedSheet, 'MANO DE OBRA', 'MATERIALES', True, True), (0, 2, 4))
     _new_labour_list = transform_tuples(clean_labour_dict, _labour_list)
 
-    _materials_list = clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'MATERIALES', 'TRANSPORTE', True, True), (1, 2, 3, 4))
+    _materials_list = clean_list_tuples(get_tuples_between_tags(    
+        _detailedSheet, 'MATERIALES', 'TRANSPORTE', True, True), (0, 1, 2, 4))
     _new_materials_list = transform_tuples(
         clean_materials_dict, _materials_list)
 
     _transport_list = clean_list_tuples(get_tuples_between_tags(
-        _detailedSheet, 'TRANSPORTE', '', True, True), (1, 2, 3, 4))
+        _detailedSheet, 'TRANSPORTE', '', True, True), (0, 1, 2, 3))
     _new_transport_list = transform_tuples(
         clean_transport_dict, _transport_list)
 
     _new_pa = {
         'SHEET': sheet,
-        'ITEM': _detailedSheet[4][1].__str__().strip(),
-        'RUBRO': _detailedSheet[3][1].__str__().strip(),
-        'UNIDAD': _detailedSheet[3][3].__str__().strip(),
+        'ITEM': _detailedSheet[1][1].__str__().strip(),
+        'RUBRO': _detailedSheet[2][1].__str__().strip(),
+        'UNIDAD': _detailedSheet[2][3].__str__().strip(),
         'EQUIPO': _new_equipment_list,
         'MANO DE OBRA': _new_labour_list,
         'MATERIALES': _new_materials_list,
